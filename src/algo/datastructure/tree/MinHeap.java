142d79055975abd521df572dd7f9dbbaaaab0938
rohitkumarbhagat <rohitkumarbhagat.in@gmail.com>
2012-04-09 10:11:32 +0530

    office

--- src/algo/datastructure/tree/MinHeap.java
+++ src/algo/datastructure/tree/MinHeap.java
@@ -1,13 +1,15 @@
 package algo.datastructure.tree;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * @author rohitkumar
  * 
  * @param <T>
  */
-public class MinHeap<T extends Comparable<T>> {
+public class MinHeap<T extends Comparable<T>>
+{
 
 	private T[] dataArray;
 
@@ -15,7 +17,8 @@ public class MinHeap<T extends Comparable<T>> {
 
 	private int capacity;
 
-	public MinHeap(Class<T> clazz, int capacity) {
+	public MinHeap(Class<T> clazz, int capacity)
+	{
 		// dataArray = (T[]) new Object[capacity];
 		// this.dataArray = new T[capacity];
 
@@ -25,9 +28,22 @@ public class MinHeap<T extends Comparable<T>> {
 
 	}
 
-	public boolean add(T data) {
+	public MinHeap(int capacity)
+	{
+		// dataArray = (T[]) new Object[capacity];
+		// this.dataArray = new T[capacity];
+
+		dataArray = (T[]) Array.newInstance(Integer.class, capacity);
+		length = 0;
+		this.capacity = capacity;
+
+	}
+
+	public boolean add(T data)
+	{
 		// if list full then no entry
-		if (data == null || isFull()) {
+		if (data == null || isFull())
+		{
 			return false;
 		}
 		// Add to end of the list
@@ -39,71 +55,84 @@ public class MinHeap<T extends Comparable<T>> {
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		StringBuilder heapStrBldr = new StringBuilder();
 
-		for (int i = 0; i < length; i++) {
+		for (int i = 0; i < length; i++)
+		{
 			heapStrBldr.append(dataArray[i].toString()).append("    ");
 		}
 		return heapStrBldr.toString();
 	}
 
-	public boolean isFull() {
-		if (length >= capacity) {
+	public boolean isFull()
+	{
+		if (length >= capacity)
+		{
 			return true;
 		}
 		return false;
 	}
 
-	private boolean heapify(int index) {
+	private boolean heapify(int index)
+	{
 		boolean result;
 		// Ensure Heap property
-		if (index > -1 && index < length) {
+		if (index > -1 && index < length)
+		{
 
 			result = true;
 			// iterate upto root
-			while (index > 0) {
+			while (index > 0)
+			{
 				int parentIndex = getParent(index);
 
-				if (dataArray[parentIndex].compareTo(dataArray[index]) == 1) {
+				if (dataArray[parentIndex].compareTo(dataArray[index]) == 1)
+				{
 					// if parent > child, swap child and parent and heapify
 					// further
 					T temp = dataArray[parentIndex];
 					dataArray[parentIndex] = dataArray[index];
 					dataArray[index] = temp;
 					index = parentIndex;
-				} else {
+				}
+				else
+				{
 					// heap property maintained as parent > child
 					break;
 				}
 			}
 
-		} else {
+		}
+		else
+		{
 			result = false;
 		}
 		return result;
 
 	}
 
-	private boolean percolate(int index) {
+	private boolean percolate(int index)
+	{
 		boolean result;
-		if (index > -1 && index < length) {
+		if (index > -1 && index < length)
+		{
 			int smallest = index;
 			// get left and right child if exists
 
-			if (getLeftChild(index) != -1
-					&& dataArray[smallest]
-							.compareTo(dataArray[getLeftChild(index)]) == 1) {
+			if (getLeftChild(index) != -1 && dataArray[smallest].compareTo(dataArray[getLeftChild(index)]) == 1)
+			{
 				// if smallest is > left child
 				smallest = getLeftChild(index);
 			}
-			if (getRightChild(index) != -1
-					&& dataArray[smallest]
-							.compareTo(dataArray[getRightChild(index)]) == 1) {
+			if (getRightChild(index) != -1 && dataArray[smallest].compareTo(dataArray[getRightChild(index)]) == 1)
+			{
 				// if smallest is > right child
 				smallest = getRightChild(index);
 			}
-			if (smallest != index) {
+			if (smallest != index)
+			{
 				// swap index and small
 				T temp = dataArray[smallest];
 				dataArray[smallest] = dataArray[index];
@@ -113,7 +142,9 @@ public class MinHeap<T extends Comparable<T>> {
 			}
 			return true;
 
-		} else {
+		}
+		else
+		{
 			result = false;
 		}
 		return result;
@@ -124,13 +155,18 @@ public class MinHeap<T extends Comparable<T>> {
 	 * @return index of Parent. If root then 0 , if oout of bound then -1
 	 * 
 	 */
-	private int getParent(int childIndex) {
-		if (childIndex < length && childIndex > -1) {
-			if (childIndex == 0) {
+	private int getParent(int childIndex)
+	{
+		if (childIndex < length && childIndex > -1)
+		{
+			if (childIndex == 0)
+			{
 				return 0;
 			}
 			return (childIndex - 1) / 2;
-		} else {
+		}
+		else
+		{
 			return -1;
 		}
 	}
@@ -139,12 +175,17 @@ public class MinHeap<T extends Comparable<T>> {
 	 * @param parentIndex
 	 * @return -1 if left child does not exist or returns index of left child
 	 */
-	private int getLeftChild(int parentIndex) {
-		if (parentIndex > -1 && parentIndex < length) {
+	private int getLeftChild(int parentIndex)
+	{
+		if (parentIndex > -1 && parentIndex < length)
+		{
 			int leftChildIndex = 2 * parentIndex + 1;
-			if (leftChildIndex < length) {
+			if (leftChildIndex < length)
+			{
 				return leftChildIndex;
-			} else {
+			}
+			else
+			{
 				return -1;
 			}
 		}
@@ -153,12 +194,17 @@ public class MinHeap<T extends Comparable<T>> {
 		}
 	}
 
-	private int getLeftChild(int parentIndex, int length) {
-		if (parentIndex > -1 && parentIndex < length) {
+	private int getLeftChild(int parentIndex, int length)
+	{
+		if (parentIndex > -1 && parentIndex < length)
+		{
 			int leftChildIndex = 2 * parentIndex + 1;
-			if (leftChildIndex < length) {
+			if (leftChildIndex < length)
+			{
 				return leftChildIndex;
-			} else {
+			}
+			else
+			{
 				return -1;
 			}
 		}
@@ -167,41 +213,57 @@ public class MinHeap<T extends Comparable<T>> {
 		}
 	}
 
-	private int getRightChild(int parentIndex) {
-		if (parentIndex > -1 && parentIndex < length) {
+	private int getRightChild(int parentIndex)
+	{
+		if (parentIndex > -1 && parentIndex < length)
+		{
 			int rightChildindex = 2 * parentIndex + 2;
-			if (rightChildindex < length) {
+			if (rightChildindex < length)
+			{
 				return rightChildindex;
-			} else {
+			}
+			else
+			{
 				// Right child out of index
 
 				return -1;
 			}
-		} else {
+		}
+		else
+		{
 			// out of range parent
 			return -1;
 		}
 	}
 
-	private int getRightChild(int parentIndex, int length) {
-		if (parentIndex > -1 && parentIndex < length) {
+	private int getRightChild(int parentIndex, int length)
+	{
+		if (parentIndex > -1 && parentIndex < length)
+		{
 			int rightChildindex = 2 * parentIndex + 2;
-			if (rightChildindex < length) {
+			if (rightChildindex < length)
+			{
 				return rightChildindex;
-			} else {
+			}
+			else
+			{
 				// Right child out of index
 
 				return -1;
 			}
-		} else {
+		}
+		else
+		{
 			// out of range parent
 			return -1;
 		}
 	}
 
-	public void sort() {
+	public void sort()
+	{
 		int tempLength = length;
-		while (length > 1) {
+		while (length > 1)
+		{
 			// swap first and last element
 			T temp = dataArray[0];
 			dataArray[0] = dataArray[length - 1];
@@ -215,14 +277,17 @@ public class MinHeap<T extends Comparable<T>> {
 		length = tempLength;
 	}
 
-	public T deleteElement(T element) {
+	public T deleteElement(T element)
+	{
 		return deleteIndex(indexOf(element));
 
 	}
 
-	public T deleteIndex(int index) {
+	public T deleteIndex(int index)
+	{
 		T deletedNode = null;
-		if (index > -1 && index < length) {
+		if (index > -1 && index < length)
+		{
 			// index within range
 
 			// swap with last element
@@ -240,13 +305,52 @@ public class MinHeap<T extends Comparable<T>> {
 
 	}
 
-	private int indexOf(T element) {
-		for (int i = 0; i < length; i++) {
-			if (dataArray[i].equals(element)) {
+	private int indexOf(T element)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			if (dataArray[i].equals(element))
+			{
 				return i;
 			}
 		}
 		return -1;
 	}
 
+	public T getMin()
+	{
+		if (length > 0)
+		{
+			return dataArray[0];
+		}
+		return null;
+	}
+
+	public T getLeft()
+	{
+
+	}
+
+	public T getKthSmallest(int k)
+	{
+		T kthSmallest = null;
+		if (k > 0 && k <= length)
+		{
+			// kth smallest element exist
+			int count = 0;
+			MinHeap<T> tempHeap = new MinHeap<T>(k + 1);
+			// 0 element will exist
+			tempHeap.add(dataArray[0]);
+			while(true){
+				kthSmallest = tempHeap.getMin();
+				if(++count==k){
+					return kthSmallest;
+				}
+				tempHeap.add(kthSmallest.)
+				
+			}
+
+		}
+		return kthSmallest;
+	}
 }
