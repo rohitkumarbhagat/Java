8ead3d310ea1d8367e468ffcbd57ab7e32daf6e1
rohitkumarbhagat <rohitkumarbhagat.in@gmail.com>
2012-06-20 19:37:45 +0530

    office:knapsack

---
+++ src/algo/dp/KnapsackProblems.java
@@ -0,0 +1,53 @@
+package algo.dp;
+
+public class KnapsackProblems {
+
+	/**
+	 * @param sizes
+	 *            : sizes of different stuff
+	 * @param weights
+	 *            : weights of different stuff
+	 * @param capacity
+	 *            : capacity of container
+	 * @return
+	 */
+	int unboundedKnapsack(int[] sizes, int[] weights, int capacity) {
+		// stores optimal sizes of different capacity
+		int[] optimalList = new int[capacity + 1];
+		int[] parentArray = new int[capacity + 1];
+
+		for (int i = 0; i <= capacity; i++) {
+			optimalList[i] = 0;
+			parentArray[i] = 0;
+			for (int j = 0; j < sizes.length; j++) {
+				if (i >= sizes[j]
+						&& optimalList[i] < weights[j]
+								+ optimalList[i - sizes[j]]) {
+					optimalList[i] = weights[j] + optimalList[i - sizes[j]];
+					parentArray[i] = sizes[j];
+				}
+			}
+		}
+		printSelectedSizes(parentArray, capacity);
+		return optimalList[capacity];
+
+	}
+
+	void printSelectedSizes(int[] parentArray, int capacity) {
+		while (capacity > 0 && parentArray[capacity] > 0) {
+			System.out.print(parentArray[capacity]);
+			capacity -= parentArray[capacity];
+		}
+		System.out.println();
+	}
+	
+	int zeroOneKnapSack(int[] sizes, int[] weights, int capacity){
+		int[] lastRow=new int[capacity+1];
+		int[] currentRow=new int[capacity+1];
+		int currentSizeIndex=0;
+		for(int i=0;i<sizes.length)
+		return currentRow[capacity];
+		
+	}
+
+}
---
+++ testsrc/algo/dp/KnapsackProblemsTest.java
@@ -0,0 +1,35 @@
+package algo.dp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KnapsackProblemsTest {
+
+	private KnapsackProblems knapsack = new KnapsackProblems();
+
+	@Test
+	public void test() {
+		int[] sizes={2,4,7,9};
+		int[] weights={1,6,2,9};
+		
+		assertEquals(13, knapsack.unboundedKnapsack(sizes, weights, 10));
+		assertEquals(0, knapsack.unboundedKnapsack(sizes, weights, 0));
+		assertEquals(0, knapsack.unboundedKnapsack(sizes, weights, 1));
+		assertEquals(6, knapsack.unboundedKnapsack(sizes, weights, 5));
+		assertEquals(36, knapsack.unboundedKnapsack(sizes, weights, 25));
+	}
+	
+	@Test
+	public void test1() {
+		int[] sizes={2,5,12,23};
+		int[] weights={1,6,7,9};
+		
+		assertEquals(12, knapsack.unboundedKnapsack(sizes, weights, 10));
+		assertEquals(0, knapsack.unboundedKnapsack(sizes, weights, 0));
+		assertEquals(0, knapsack.unboundedKnapsack(sizes, weights, 1));
+		assertEquals(6, knapsack.unboundedKnapsack(sizes, weights, 5));
+		assertEquals(30, knapsack.unboundedKnapsack(sizes, weights, 25));
+	}
+
+}
